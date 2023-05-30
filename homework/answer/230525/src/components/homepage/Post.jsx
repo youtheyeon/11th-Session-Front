ba6410e7180cb4126288c331c0f92075ee772f07
서진 <dltjwls0529@gmail.com>
2023-05-30 16:13:09 +0900

    [230525] React(2) 스터디 정답 코드

---
+++ homework/answer/230525/src/components/homepage/Post.jsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+// props로 받아올 post 구조 분해 할당
+const Post = ({ post }) => {
+	// post의 제목과 내용 구조 분해 할당
+	const { title, content } = post;
+
+	// 제목과 내용 리턴 안에 넣어 렌더링
+	return (
+		<PostContainer>
+			<Title>{title}</Title>
+			<Content>{content}</Content>
+		</PostContainer>
+	);
+};
+
+export default Post;
+
+const PostContainer = styled.div`
+	width: 90%;
+	margin: 20px;
+`;
+
+const Title = styled.div`
+	font-size: 1.5rem;
+	font-weight: 600;
+	margin-bottom: 10px;
+`;
+
+const Content = styled.div``;
